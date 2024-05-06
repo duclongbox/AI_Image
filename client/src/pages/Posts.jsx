ebc22e96ce1344fac3f374217756ed480974c1fb
DLong <duclongbox@gmail.com>
2024-05-06 18:42:45 -0400

    create post

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FormField,Loader } from '../components'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Posts = () => {
+  const navigate = useNavigate()
+  const [form,setForm] = useState({
+    name:'',
+    prompt:'',
+    photo:''
+  })
+  const [generateImg,setgenerateImg] = useState(false)
+  const [loading,setLoading] = useState(false)
+
   return (
-    <div>
-      Post
-    </div>
+    <section className='max-w-7x1 mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[32px]'> Create</h1>
+      </div>
+    </section>
   )
 }
 
