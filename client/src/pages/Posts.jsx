7795b29b5447e278a0b1e3a5b8a72f0ec4db3a39
DLong <duclongbox@gmail.com>
2024-05-07 11:37:04 -0400

    create post

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import {React,useState} from 'react'
+import { preview } from '../assets'
 import { FormField,Loader } from '../components'
 import { useNavigate } from 'react-router-dom'
 
@@ -19,6 +20,7 @@ const Posts = () => {
   const handleChange = (e) => {
 
   }
+  const createImage = () => {}
   return (
     <section className='max-w-7x1 mx-auto'>
       <div>
@@ -27,8 +29,29 @@ const Posts = () => {
       </div>
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
-          <FormField lableName="name" type="text" name="name" placeholder="Long Hoang" value={form.name} handleChange={handleChange} />
-          <FormField lableName="prompt" type="text" name="prompt" placeholder="Long Hoang" value={form.prompt} handleChange={handleChange} />
+          <FormField labelName="name" type="text" name="name" placeholder="Long Hoang" value={form.name} handleChange={handleChange} />
+          <FormField labelName="prompt" type="text" name="prompt" placeholder="Write something" value={form.prompt} handleChange={handleChange} />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {
+              form.photo ? (
+                <img src={form.photo} alt={form.prompt} className='w-full h-full object-contain'></img>
+              ):( <img src={preview} alt="preview" className='w-9/12 h-9/12 object-contain opacity-40 ' ></img>)
+            }
+            {
+              generateImg ? ( <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+              <Loader />
+            </div>)  : null
+            }
+          </div>
+        </div>
+        <div className='mt-5 flex gap-5'>
+          <button
+              type="button"
+              onClick={createImage}
+              className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {generateImg ? 'Generating...' : 'Generate'}
+          </button>
         </div>
       </form>
     </section>
